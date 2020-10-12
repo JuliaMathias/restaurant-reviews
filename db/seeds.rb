bacdbadeec54c83082d2337e2d82b81fe7467dec
Julia Mathias <juliamss@gmail.com>
2020-10-12 20:56:39 -0300

    added chef name to restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+ # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -10,8 +10,8 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5, chef_name: 'Adriana Mathias' }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4, chef_name: 'Julia Mathias' }
 
 [ dishoom, pizza_east ].each do |attributes|
   restaurant = Restaurant.create!(attributes)
@@ -23,6 +23,7 @@ puts "Finished!"
   Restaurant.create(
     name: Faker::Restaurant.name,
     address:Faker::Address.street_address,
-    stars: rand(1..5)
+    stars: rand(1..5),
+    chef_name: ['Gordon Ramsey', 'Gido', 'Alain Ducasse', 'Jamie Oliver'].sample
     )
 end
